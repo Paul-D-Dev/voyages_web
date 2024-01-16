076f9cee8050d3e16dd80914ff05ecd3d71b133f
Paul DAO <dao.van.paul@gmail.com>
2024-01-16 14:20:53 -0500

    FEAT component nav bar

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    RouterLink,
+  ],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent {
+
+}
