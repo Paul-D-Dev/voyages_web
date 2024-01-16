42638b3a260aa32ea8a64f672c0b7bce8f651a93
Paul DAO <dao.van.paul@gmail.com>
2024-01-16 15:17:16 -0500

    FEAT create map service to init map

---
+++ src/app/shared/services/map.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import * as L from 'leaflet';
+import { LatLngTuple } from 'leaflet';
+import { IGpsPosition } from "../interfaces/gps-position.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+
+  initMap(position: IGpsPosition, idMap = 'map') {
+    const center: LatLngTuple = [position.lat, position.long];
+    const map = L.map(idMap, {
+      center,
+      zoom: 16
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(map);
+
+    return map;
+  }
+}
