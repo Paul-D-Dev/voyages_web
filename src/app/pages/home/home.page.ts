abf626a26108b3c0e986ad3428f28d69546a627f
Paul DAO <dao.van.paul@gmail.com>
2024-01-16 15:19:04 -0500

    UPDATE home page calls map component

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MapComponent } from "../../components/map/map.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MapComponent
+  ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
