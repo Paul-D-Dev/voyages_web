30af993ddeccb6ce722b9e99a7e903e1562693dc
Paul DAO <dao.van.paul@gmail.com>
2024-01-16 17:42:26 -0500

    FEAT home page calls search address form

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MapComponent } from "../../components/map/map.component";
+import { SearchAddressFormComponent } from "../../components/search-address-form/search-address-form.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MapComponent
+    MapComponent,
+    SearchAddressFormComponent,
   ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
