a52129d2a4c299de83ed164934f975fe612c8679
Paul DAO <dao.van.paul@gmail.com>
2024-01-18 14:42:23 -0500

    FEAT component create travel page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomePage } from "./pages/home/home.page";
+import { TravelsPage } from "./pages/travels/travels.page";
+import { AddTravelPage } from "./pages/travels/add-travel/add-travel.page";
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomePage
+    component: HomePage,
+  },
+  {
+    path: 'travels',
+    component: TravelsPage
+  },
+  {
+    path: 'travels/add',
+    component: AddTravelPage
   }
 ];
---
+++ src/app/pages/travels/travels.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-travels',
+  standalone: true,
+  imports: [],
+  templateUrl: './travels.page.html',
+  styleUrl: './travels.page.scss'
+})
+export class TravelsPage {
+
+}
