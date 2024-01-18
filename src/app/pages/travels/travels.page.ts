7abd7c49f79b33a2201a6fe8e26309a0cffc894d
Paul DAO <dao.van.paul@gmail.com>
2024-01-18 17:47:07 -0500

    FEAT travels page add list travel item

--- src/app/pages/travels/travels.page.ts
+++ src/app/pages/travels/travels.page.ts
@@ -1,15 +1,119 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { ITravel } from "../../shared/interfaces/travel.interface";
+import { DatePipe } from "@angular/common";
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-travels',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    DatePipe,
+    MatButton
   ],
   templateUrl: './travels.page.html',
   styleUrl: './travels.page.scss'
 })
 export class TravelsPage {
+  travelListMock: ITravel[] = [
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+    {
+      id: 1,
+      name: 'Hawaii',
+      dateStart: new Date('10/01/2023'),
+      dateEnd: new Date('10/15/2023'),
+    },
+  ];
+  travelList: ITravel[] = this.travelListMock || [];
 
 }
