b0d62a112049bf5417456273f01e3d0cd0a03487
Paul DAO <dao.van.paul@gmail.com>
2024-01-18 15:41:41 -0500

    WIP travel page button to route to add travel

--- src/app/pages/travels/travels.page.ts
+++ src/app/pages/travels/travels.page.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-travels',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './travels.page.html',
   styleUrl: './travels.page.scss'
 })
