e83cd81a8926bbdd6c09157d5d848c4c601b26be
Paul DAO <dao.van.paul@gmail.com>
2024-01-18 17:40:54 -0500

    UPDATE navigation service add router fn go

--- src/app/shared/services/navigation.service.ts
+++ src/app/shared/services/navigation.service.ts
@@ -1,14 +1,24 @@
 import { inject, Injectable } from '@angular/core';
 import { Location } from "@angular/common";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
   location = inject(Location);
+  router = inject(Router);
 
   back() {
     this.location.back();
+  }
+
+  goUrl(path: string) {
+    this.router.navigateByUrl(path);
+  }
+
+  go(path: string[]) {
+    this.router.navigate(path);
   }
 
 }
