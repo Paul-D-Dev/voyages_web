4ea0bd63a1ab87e2145317f50a48cc9f58c72d81
Paul DAO <dao.van.paul@gmail.com>
2024-01-18 15:41:03 -0500

    UPDATE app config: provide mat form fields options outline and moment date adapter

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from "@angular/common/http";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
+import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' }
+    },
+    provideMomentDateAdapter(undefined, { useUtc: true })
+  ]
 };
