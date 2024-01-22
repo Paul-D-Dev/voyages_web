feb7eafd3af5f0f5dc4b531a02dc1593d06b24ea
Paul DAO <dao.van.paul@gmail.com>
2024-01-22 15:56:34 -0500

    WIP home page try to update searchFocused$ when search params changed

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MapComponent } from "../../components/map/map.component";
 import { SearchAddressFormComponent } from "../../components/search-address-form/search-address-form.component";
+import { ActivatedRoute } from "@angular/router";
+import { map, Observable } from "rxjs";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
   selector: 'app-home',
@@ -8,10 +11,24 @@ import { SearchAddressFormComponent } from "../../components/search-address-form
   imports: [
     MapComponent,
     SearchAddressFormComponent,
+    AsyncPipe,
   ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
 export class HomePage {
+  routes = inject(ActivatedRoute);
+  searchFocused$: Observable<boolean | null> = this.getParamsSearchValue();
+
+
+  getParamsSearchValue(): Observable<boolean> {
+    console.log('call');
+    return this.routes.queryParams.pipe(
+      map(params => {
+        console.log(params);
+        return params['search'];
+      })
+    );
+  }
 
 }
