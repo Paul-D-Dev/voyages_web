68aace41a20bda425887bc2d104952cc2a9ef411
Paul DAO <dao.van.paul@gmail.com>
2024-01-22 15:28:33 -0500

    UPDATE icons enum add location_on

--- src/app/shared/enums/icons.enum.ts
+++ src/app/shared/enums/icons.enum.ts
@@ -2,6 +2,7 @@ export enum Icons {
   ARROW_BACK = 'arrow_back',
   ARROW_FORWARD = 'arrow_forward',
   HOME = 'home',
+  LOCATION_ON = 'location_on',
   ROUTE = 'route',
   SETTINGS = 'settings'
 }
