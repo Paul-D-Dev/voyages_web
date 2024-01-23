47fc44b5e2faf725bf322f45596447b486813d17
Paul DAO <dao.van.paul@gmail.com>
2024-01-23 16:02:23 -0500

    FEAT Travel Service sync with local storage

--- src/app/shared/services/travel.service.ts
+++ src/app/shared/services/travel.service.ts
@@ -1,18 +1,25 @@
 import { Injectable, signal, WritableSignal } from '@angular/core';
 import { ITravel, ITravelFormData } from "../interfaces/travel.interface";
+import { LocalStorageService } from "./local-storage.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TravelService {
+  constructor(private localStorageService: LocalStorageService<ITravel[]>) {
+    this.localStorageService.loadStateFromLocalStorage(this.featureKey, this.travels, []);
+  }
 
-  travels: WritableSignal<ITravel[]> = signal<ITravel[]>([]);
+  private readonly featureKey = 'travels';
+  readonly travels: WritableSignal<ITravel[]> = signal<ITravel[]>([]);
 
   add(travel: ITravelFormData): void {
     this.travels.update((travels) => {
-      const newId = travels[travels.length - 1].id + 1;
-      return [...travels, { id: newId, ...travel }];
+      const length = travels.length;
+      const id = length === 0 ? 1 : travels[length - 1].id + 1;
+      return [...travels, { id, ...travel }];
     });
+    this.localStorageService.set(this.featureKey, this.travels());
   }
 
   update(travelData: ITravelFormData, id: number): void {
@@ -20,6 +27,7 @@ export class TravelService {
       travels.map(travel => travel.id === id ?
         { id: travel.id, ...travelData } : travel
       ));
+    this.localStorageService.set(this.featureKey, this.travels());
   }
 
 }
