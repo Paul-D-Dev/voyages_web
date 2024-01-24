d97e8d3ef98758583c4f1a7ce1e490e1dc58ceeb
Paul DAO <dao.van.paul@gmail.com>
2024-01-24 14:37:37 -0500

    UPDATE icon enum add north west

--- src/app/shared/enums/icons.enum.ts
+++ src/app/shared/enums/icons.enum.ts
@@ -3,6 +3,7 @@ export enum Icons {
   ARROW_FORWARD = 'arrow_forward',
   HOME = 'home',
   LOCATION_ON = 'location_on',
+  NORTH_WEST = 'north_west',
   ROUTE = 'route',
   SETTINGS = 'settings'
 }
