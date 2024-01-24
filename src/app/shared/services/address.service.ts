61e1ea06f40f056c6ed2971ecade726c3c06bbd5
Paul DAO <dao.van.paul@gmail.com>
2024-01-24 11:23:10 -0500

    FEAT service address moves lookUp fn

---
+++ src/app/shared/services/address.service.ts
@@ -0,0 +1,28 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { map, Observable, of } from "rxjs";
+import { IAddress, IAddressResponseAPI } from "../interfaces/address.interface";
+import { Address } from "../models/address.model";
+import { environment } from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+
+  private _http = inject(HttpClient);
+  private readonly geoSearchAPI = environment.geoSearchAPI;
+
+  lookUp(query: string | null): Observable<IAddress[]> {
+    if (query) {
+      const url = `${this.geoSearchAPI}/search?q=${query}&format=json`;
+      return this._http.get<IAddressResponseAPI[]>(url).pipe(
+        map((result) => {
+          return result.map(address => new Address((address)));
+        })
+      );
+    } else {
+      return of([]);
+    }
+  }
+}
