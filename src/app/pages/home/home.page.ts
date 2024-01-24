a3fd73a1e86dda10ef41465637617aab67556873
Paul DAO <dao.van.paul@gmail.com>
2024-01-24 15:11:55 -0500

    UPDATE home page onSelectedAddress calls addMarker then setView

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -5,6 +5,7 @@ import { GlobalStateService } from "../../shared/services/global-state.service";
 import { JsonPipe } from "@angular/common";
 import { IAddress } from "../../shared/interfaces/address.interface";
 import { AddressListComponent } from "../../components/address-list/address-list.component";
+import { MapService } from "../../shared/services/map.service";
 import { IGpsPosition } from "../../shared/interfaces/gps-position.interface";
 
 @Component({
@@ -23,24 +24,17 @@ export class HomePage {
   globalStateService = inject(GlobalStateService);
   searchFocused = inject(GlobalStateService).select("isSearchFocused");
   addressList: IAddress[] = [];
-  // marker: WritableSignal<IGpsPosition> = signal({
-  //   lat: 45.5031824,
-  //   lng: -73.5698065
-  // });
+  mapService = inject(MapService);
 
   getLookupAddress(addresses: IAddress[]) {
     this.addressList = addresses;
   }
 
-  getSelectedAddress(address: IAddress): void {
+  onSelectedAddress(address: IAddress): void {
     const { lat, lng } = address;
-    const position: IGpsPosition = {
-      lat,
-      lng
-    };
-    // TODO addMarker to maps
-    console.log(position);
-    // this.marker.set(position);
+    const position: IGpsPosition = { lat, lng };
     this.globalStateService.set("isSearchFocused", false);
+    this.mapService.addMarker(position);
+    this.mapService.setView(position);
   }
 }
