e410c1ff42f7df326f458d166bf77b53d05f60a9
Paul DAO <dao.van.paul@gmail.com>
2024-01-24 11:15:43 -0500

    UPDATE home page get addressResult from search address form

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { MapComponent } from "../../components/map/map.component";
 import { SearchAddressFormComponent } from "../../components/search-address-form/search-address-form.component";
 import { GlobalStateService } from "../../shared/services/global-state.service";
+import { JsonPipe } from "@angular/common";
+import { IAddress } from "../../shared/interfaces/address.interface";
 
 @Component({
   selector: 'app-home',
@@ -9,10 +11,16 @@ import { GlobalStateService } from "../../shared/services/global-state.service";
   imports: [
     MapComponent,
     SearchAddressFormComponent,
+    JsonPipe,
   ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
 export class HomePage {
   searchFocused = inject(GlobalStateService).select("isSearchFocused");
+  addressList: IAddress[] = [];
+
+  getLookupAddress(addresses: IAddress[]) {
+    this.addressList = addresses;
+  }
 }
