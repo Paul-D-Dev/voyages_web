a4782d0c9d9eb1135794f502f1155879e58fcd96
Paul DAO <dao.van.paul@gmail.com>
2024-01-24 14:42:56 -0500

    UPDATE home page uses app-address-list and get position address

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,6 +4,8 @@ import { SearchAddressFormComponent } from "../../components/search-address-form
 import { GlobalStateService } from "../../shared/services/global-state.service";
 import { JsonPipe } from "@angular/common";
 import { IAddress } from "../../shared/interfaces/address.interface";
+import { AddressListComponent } from "../../components/address-list/address-list.component";
+import { IGpsPosition } from "../../shared/interfaces/gps-position.interface";
 
 @Component({
   selector: 'app-home',
@@ -12,15 +14,33 @@ import { IAddress } from "../../shared/interfaces/address.interface";
     MapComponent,
     SearchAddressFormComponent,
     JsonPipe,
+    AddressListComponent,
   ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
 export class HomePage {
+  globalStateService = inject(GlobalStateService);
   searchFocused = inject(GlobalStateService).select("isSearchFocused");
   addressList: IAddress[] = [];
+  // marker: WritableSignal<IGpsPosition> = signal({
+  //   lat: 45.5031824,
+  //   lng: -73.5698065
+  // });
 
   getLookupAddress(addresses: IAddress[]) {
     this.addressList = addresses;
+  }
+
+  getSelectedAddress(address: IAddress): void {
+    const { lat, lng } = address;
+    const position: IGpsPosition = {
+      lat,
+      lng
+    };
+    // TODO addMarker to maps
+    console.log(position);
+    // this.marker.set(position);
+    this.globalStateService.set("isSearchFocused", false);
   }
 }
