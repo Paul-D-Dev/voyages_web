ab49c30e3bd4b872483001d6d03503d89b172165
Paul DAO <dao.van.paul@gmail.com>
2024-01-25 15:29:11 -0500

    UPDATE travel interface add travel detail with step

--- src/app/shared/interfaces/travel.interface.ts
+++ src/app/shared/interfaces/travel.interface.ts
@@ -1,3 +1,5 @@
+import { IGpsPosition } from "./gps-position.interface";
+
 export interface ITravel {
   id: number,
   name: string,
@@ -6,3 +8,21 @@ export interface ITravel {
 }
 
 export type ITravelFormData = Omit<ITravel, 'id'>
+
+export interface ITravelDetail extends ITravel {
+  steps: ITravelStep[];
+}
+
+export interface ITravelStep {
+  id: number,
+  order: number,
+  label: string,
+  category: string, // Flight, Road, Hotel / Rest, Restaurant, Party, Activities
+  description?: string,
+  location: IGpsPosition,
+  dateStart: Date,
+  dateEnd: Date
+  createdDate: Date,
+  updatedDate?: Date,
+  // customIcon
+}
