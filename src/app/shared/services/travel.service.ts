1e0bbcd3ea260caa455503df7f7774eb32fb9e00
Paul DAO <dao.van.paul@gmail.com>
2024-01-29 16:16:18 -0500

    UPDATE travel service: create new fn getById + updateStepIndexes

--- src/app/shared/services/travel.service.ts
+++ src/app/shared/services/travel.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal, WritableSignal } from '@angular/core';
-import { ITravel, ITravelFormData } from "../interfaces/travel.interface";
+import { ITravel, ITravelFormData, ITravelStep } from "../interfaces/travel.interface";
 import { LocalStorageService } from "./local-storage.service";
 import { LocalStorageKeyEnum } from "../enums/local-storage-key.enum";
 
@@ -14,21 +14,45 @@ export class TravelService {
   private readonly localStorageKey = LocalStorageKeyEnum.TRAVELS;
   readonly travels: WritableSignal<ITravel[]> = signal<ITravel[]>([]);
 
-  add(travel: ITravelFormData): void {
-    this.travels.update((travels) => {
-      const length = travels.length;
-      const id = length === 0 ? 1 : travels[length - 1].id + 1;
-      return [...travels, { id, ...travel }];
-    });
+  private _saveLocal() {
     this.localStorageService.set(this.localStorageKey, this.travels());
+  }
+
+  add(travel: ITravelFormData): void {
+    this.travels.update((travelList: ITravel[]) => {
+      const length = travelList.length;
+      const id = length === 0 ? 1 : travelList[length - 1].id + 1;
+      return [...travelList, { id, ...travel, steps: [] }];
+    });
+    this._saveLocal();
+  }
+
+  getById(travelId: number): ITravel | undefined {
+    return this.travels().find(t => t.id === travelId);
   }
 
   update(travelData: ITravelFormData, id: number): void {
     this.travels.update(travels =>
       travels.map(travel => travel.id === id ?
-        { id: travel.id, ...travelData } : travel
+        { id: travel.id, ...travelData, steps: travel.steps } : travel
       ));
-    this.localStorageService.set(this.localStorageKey, this.travels());
+    this._saveLocal();
+  }
+
+  updateStepIndexes(steps: ITravelStep[], travelId: number): void {
+    const replaceStepsById = (array: ITravel[], id: number): ITravel[] =>
+      array.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            steps
+          };
+        } else {
+          return item;
+        }
+      });
+    this.travels.update((travels) => replaceStepsById(travels, travelId));
+    this._saveLocal();
   }
 
 }
