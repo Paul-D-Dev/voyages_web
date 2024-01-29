f838e337075bcc9d0a6e0f82f64459f8abc5bf7e
Paul DAO <dao.van.paul@gmail.com>
2024-01-29 16:34:28 -0500

    UPDATE travel route add-step

--- src/app/pages/travels/travel.route.ts
+++ src/app/pages/travels/travel.route.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { TravelsPage } from "./travels.page";
 import { TravelDetailPage } from "./travel-detail/travel-detail.page";
 import { AddTravelPage } from "./add-travel/add-travel.page";
+import { AddStepPage } from "./add-step/add-step.page";
 
 export const travelRoutes: Routes = [
   {
@@ -16,5 +17,8 @@ export const travelRoutes: Routes = [
     path: ':id',
     component: TravelDetailPage
   },
-
+  {
+    path: ':id/add-step',
+    component: AddStepPage,
+  },
 ];
