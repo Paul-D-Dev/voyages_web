b119b5d3801f433d99fb121b386791cf743137f9
Paul DAO <dao.van.paul@gmail.com>
2024-01-30 14:30:46 -0500

    UPDATE form add travel step add location input with auto complete addresses

--- src/app/components/form-add-travel-step/form-add-travel-step.component.ts
+++ src/app/components/form-add-travel-step/form-add-travel-step.component.ts
@@ -14,6 +14,16 @@ import { MatInput } from "@angular/material/input";
 import { MatMiniFabButton } from "@angular/material/button";
 import { ITravelStepFormData } from "../../shared/interfaces/travel.interface";
 import { IGpsPosition } from "../../shared/interfaces/gps-position.interface";
+import {
+  MatAutocomplete,
+  MatAutocompleteSelectedEvent,
+  MatAutocompleteTrigger,
+  MatOption
+} from "@angular/material/autocomplete";
+import { debounceTime, Observable, of, startWith, switchMap } from "rxjs";
+import { AddressService } from "../../shared/services/address.service";
+import { IAddress } from "../../shared/interfaces/address.interface";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
   selector: 'app-form-add-travel-step',
@@ -32,12 +42,25 @@ import { IGpsPosition } from "../../shared/interfaces/gps-position.interface";
     MatMiniFabButton,
     MatStartDate,
     MatSuffix,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatAutocomplete,
+    MatAutocompleteTrigger,
+    MatOption,
+    AsyncPipe
   ],
   templateUrl: './form-add-travel-step.component.html',
   styleUrl: './form-add-travel-step.component.scss'
 })
 export class FormAddTravelStepComponent {
+
+  constructor(private addressService: AddressService) {
+    this.addresses$ = this.form.controls['locationAddress'].valueChanges.pipe(
+      startWith(''),
+      debounceTime(1000),
+      switchMap((value) => this.addressService.lookUp(value)),
+    );
+  }
+
   @Input() formData: ITravelStepFormData = {
     label: '',
     category: '',
@@ -49,6 +72,7 @@ export class FormAddTravelStepComponent {
 
   protected readonly Icons = Icons;
   readonly fb = inject(FormBuilder);
+  addresses$: Observable<IAddress[]> = of([]);
 
   // TODO complete FormGroup type
   form: FormGroup = this.fb.group({
@@ -57,11 +81,26 @@ export class FormAddTravelStepComponent {
     dateStart: new FormControl(this.formData.dateStart),
     dateEnd: new FormControl(this.formData.dateEnd),
     category: new FormControl(this.formData.category),
+    locationAddress: new FormControl(''),
     location: new FormGroup({
       lng: new FormControl(this.formData.location.lng),
       lat: new FormControl(this.formData.location.lat)
     })
   });
+
+  displayFn(address: IAddress): string {
+    return address && address.label ? address.label : '';
+  }
+
+  onOptionAddressSelected(ev: MatAutocompleteSelectedEvent) {
+    const address: IAddress = ev.option.value;
+    this.form.patchValue({
+      location: {
+        lng: address.lng,
+        lat: address.lat
+      }
+    });
+  }
 
   onSubmit() {
     console.log('onCLick submit button');
