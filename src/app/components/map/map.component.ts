87bad42a6055d82849ba18de0f5a0bfe267a436e
Paul DAO <dao.van.paul@gmail.com>
2024-02-01 14:52:40 -0500

    FEAT map component showMarkers fn to display the multiple steps from trip

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -2,7 +2,6 @@ import { Component, inject, Input, OnInit } from '@angular/core';
 import { IGpsPosition } from "../../shared/interfaces/gps-position.interface";
 import { MapService } from "../../shared/services/map.service";
 
-
 @Component({
   selector: 'app-map',
   standalone: true,
@@ -12,13 +11,24 @@ import { MapService } from "../../shared/services/map.service";
 })
 export class MapComponent implements OnInit {
   @Input() initPosition: IGpsPosition | undefined;
+  @Input() markers: IGpsPosition[] | undefined;
   @Input() isMarker: boolean = false;
   private _mapService = inject(MapService);
 
   ngOnInit() {
     // TODO when mapDataRoute provide value to initPosition add marker / or setView
-    this._mapService.initMap(this.initPosition, this.isMarker);
-    this._mapService.onClick();
+    this._mapService.initMap(this.initPosition);
+    this._mapService.initMapHandlers();
+    this.showMarkers(this.markers);
+  }
+
+  // TODO fix addMarker not display in the right on the map
+  protected showMarkers(markers: IGpsPosition[] | undefined): void {
+    if (markers !== undefined && markers !== null && markers.length > 0) {
+      markers.forEach(marker => {
+        this._mapService.addMarker(marker);
+      });
+    }
   }
 
 
