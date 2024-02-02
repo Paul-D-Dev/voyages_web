4f1acc89d89e816294dd856ade261e6ae12eefeb
Paul DAO <dao.van.paul@gmail.com>
2024-02-02 15:31:20 -0500

    FEAT add step page get idStep from router param to get step data

--- src/app/pages/travels/add-step/add-step.page.ts
+++ src/app/pages/travels/add-step/add-step.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input as InputRoute } from '@angular/core';
 import { HeaderComponent } from "../../../components/header/header.component";
 import { Icons } from "../../../shared/enums/icons.enum";
 import { TravelService } from "../../../shared/services/travel.service";
@@ -11,25 +11,35 @@ import { NavigationService } from "../../../shared/services/navigation.service";
   standalone: true,
   imports: [
     HeaderComponent,
-    FormAddTravelStepComponent
+    FormAddTravelStepComponent,
   ],
   templateUrl: './add-step.page.html',
   styleUrl: './add-step.page.scss'
 })
 export class AddStepPage {
+  @InputRoute('id') idTravel?: string;
+  @InputRoute('stepId') idStep?: string; // if step id means edit the step data
+
   travelService = inject(TravelService);
   navigationService = inject(NavigationService);
-  @Input('id') travelId?: string;
+
+  tripStepData: ITravelStepFormData | undefined = undefined;
   protected readonly Icons = Icons;
+
+  ngOnInit() {
+    this.tripStepData = this.travelService.getStep(+this.idTravel!, +this.idStep!);
+  }
 
 
   onSubmit(formData: ITravelStepFormData): void {
-    console.log(this.travelId);
-    if (this.travelId) {
-      this.travelService.addStep(formData, +this.travelId);
-      // if ok
-      this.navigationService.back();
+    if (this.idTravel) {
+      if (this.idStep) {
+        this.travelService.editStep(formData, +this.idTravel, +this.idStep);
+      } else {
+        this.travelService.addStep(formData, +this.idTravel);
+      }
     }
 
+    this.navigationService.back();
   }
 }
