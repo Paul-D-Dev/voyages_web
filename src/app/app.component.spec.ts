67519dcbe66b7ebfdc2c8c1d9415555a52413e0f
Paul DAO <dao.van.paul@gmail.com>
2024-02-14 17:25:51 -0500

    TEST app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { By } from "@angular/platform-browser";
+import { RouterOutlet } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, NavBarComponent, RouterTestingModule]
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
@@ -14,16 +27,28 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'voyages' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('voyages');
+  it('should contain a router outlet', () => {
+    const routerOutlet = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(routerOutlet).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, voyages');
+  it('should contain the app-nav-bar component', () => {
+    const navBarComponent = fixture.debugElement.query(By.directive(NavBarComponent));
+    expect(navBarComponent).toBeTruthy();
   });
+
+  it('should hide the nav bar if hideNavbar is true', () => {
+    component.hideNavbar = true;
+    fixture.detectChanges();
+    const navBarComponent = fixture.debugElement.query(By.directive(NavBarComponent));
+    expect(navBarComponent.nativeElement.getAttribute('hidden')).toBe("");
+  });
+
+  it('should show the nav bar if hideNavbar is false', () => {
+    component.hideNavbar = false;
+    fixture.detectChanges();
+    const navBarComponent = fixture.debugElement.query(By.directive(NavBarComponent));
+    expect(navBarComponent).toBeTruthy();
+  });
+
 });
