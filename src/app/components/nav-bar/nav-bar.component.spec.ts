1b406263f712443255f02cc857aebfa050615056
Paul DAO <dao.van.paul@gmail.com>
2024-02-16 14:46:17 -0500

    TEST nav bar with check router url match href + icon

--- src/app/components/nav-bar/nav-bar.component.spec.ts
+++ src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,16 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
 import { NavBarComponent } from "./nav-bar.component";
+import { provideRouter, Router, RouterLink } from "@angular/router";
+import { DebugElement } from "@angular/core";
+import { Icons } from "../../shared/enums/icons.enum";
 
-describe('NavBarComponent', () => {
+fdescribe('NavBarComponent', () => {
   let component: NavBarComponent;
   let fixture: ComponentFixture<NavBarComponent>;
+  let linkDes: DebugElement[];
+  let routerLinks: RouterLink[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavBarComponent, RouterTestingModule, MatIconModule]
+      imports: [NavBarComponent, RouterLink, MatIconModule],
+      providers: [provideRouter([])]
     })
       .compileComponents();
   });
@@ -19,6 +24,11 @@ describe('NavBarComponent', () => {
     fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    linkDes = fixture.debugElement.queryAll(By.directive(RouterLink));
+
+    // get attached link directive instances
+    // using each DebugElement's injector
+    routerLinks = linkDes.map((de) => de.injector.get(RouterLink));
   });
 
   it('should create the component', () => {
@@ -26,19 +36,36 @@ describe('NavBarComponent', () => {
   });
 
   it('should contain three navigation links', () => {
-    const links = fixture.debugElement.queryAll(By.css('nav.nav-bar ul li'));
-    expect(links.length).toEqual(3);
+    expect(routerLinks.length).toEqual(3);
+    expect(routerLinks[0].href).toBe('/');
+    expect(routerLinks[1].href).toBe('/travels');
+    expect(routerLinks[2].href).toBe('/settings');
   });
 
-  it('should have the correct routerLinks', () => {
-    const links = fixture.debugElement.queryAll(By.css('nav.nav-bar ul li a'));
-    expect(links[0].nativeElement.getAttribute('routerLink')).toBe('/');
-    expect(links[1].nativeElement.getAttribute('routerLink')).toBe('/travels');
-    expect(links[2].nativeElement.getAttribute('routerLink')).toBe('/settings');
-  });
+  it('can click link in template', fakeAsync(() => {
+    fixture.ngZone?.run(() => {
+      linkDes.forEach((link, index) => {
+        TestBed.inject(Router).resetConfig([{ path: '**', children: [] }]);
+        link.triggerEventHandler('click', { button: 0 });
+        tick();
+        fixture.detectChanges();
+
+        const href = routerLinks[index].href;
+        expect(TestBed.inject(Router).url).withContext(`click on link ${href!}`).toBe(href!);
+      });
+    });
+  }));
 
   it('should contain mat-icons in navigation links', () => {
     const icons = fixture.debugElement.queryAll(By.css('nav.nav-bar ul li a mat-icon'));
     expect(icons.length).toEqual(3);
+  });
+
+  it('should display the right icon for each links', () => {
+    const icons = [Icons.HOME, Icons.ROUTE, Icons.SETTINGS];
+    linkDes.forEach((link, index) => {
+      const matIconDe = link.query(By.css('mat-icon'));
+      expect(matIconDe.nativeElement.textContent).withContext('icon name').toContain(icons[index]);
+    });
   });
 });
