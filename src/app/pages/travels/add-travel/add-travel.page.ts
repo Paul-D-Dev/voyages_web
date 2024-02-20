a6dcd1c88ef33ea199fd87a8fe21efd3e65ae430
Paul DAO <dao.van.paul@gmail.com>
2024-02-20 13:29:56 -0500

    FEAT add-travel-page onSave then navigate to travel

--- src/app/pages/travels/add-travel/add-travel.page.ts
+++ src/app/pages/travels/add-travel/add-travel.page.ts
@@ -3,7 +3,7 @@ import { HeaderComponent } from "../../../components/header/header.component";
 import { FormTravelComponent } from "../../../components/form-travel/form-travel.component";
 import { ITravelFormData } from "../../../shared/interfaces/travel.interface";
 import { TravelService } from "../../../shared/services/travel.service";
-
+import { NavigationService } from "../../../shared/services/navigation.service";
 
 @Component({
   selector: 'app-add-travel',
@@ -16,12 +16,13 @@ import { TravelService } from "../../../shared/services/travel.service";
   styleUrl: './add-travel.page.scss'
 })
 export class AddTravelPage {
-
   travelService = inject(TravelService);
+  navigationService = inject(NavigationService);
 
   onSave(formData: ITravelFormData) {
     console.log(formData);
     this.travelService.add(formData);
+    this.navigationService.go(['/travels']);
   }
 
 }
