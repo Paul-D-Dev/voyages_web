50bb857cd06a59976eeeaf64ae2dc8329a7f8f95
Paul DAO <dao.van.paul@gmail.com>
2024-03-12 16:56:25 -0400

    REFACTOR home page with omnibox-search component + rename div tag classes

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,14 +3,14 @@ import { MapComponent } from "../../components/map/map.component";
 import { SearchAddressFormComponent } from "../../components/search-address-form/search-address-form.component";
 import { GlobalStateService } from "../../shared/services/global-state.service";
 import { AsyncPipe, JsonPipe } from "@angular/common";
-import { IAddress } from "../../shared/interfaces/address.interface";
 import { AddressListComponent } from "../../components/address-list/address-list.component";
 import { MapService } from "../../shared/services/map.service";
-import { IGpsPosition } from "../../shared/interfaces/gps-position.interface";
 import { TravelStateService } from "../../shared/services/travel-state.service";
 import { IMarker } from "../../shared/interfaces/marker.interface";
 import { RouterOutlet } from "@angular/router";
-import { Observable, of } from "rxjs";
+import { OmniboxSearchComponent } from "../../components/omnibox-search/omnibox-search.component";
+import { IAddress } from "../../shared/interfaces/address.interface";
+import { IGpsPosition } from "../../shared/interfaces/gps-position.interface";
 
 @Component({
   selector: 'app-home',
@@ -22,34 +22,23 @@ import { Observable, of } from "rxjs";
     AddressListComponent,
     RouterOutlet,
     AsyncPipe,
+    OmniboxSearchComponent,
   ],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
 export class HomePage {
   globalStateService = inject(GlobalStateService);
-  searchFocused = inject(GlobalStateService).select("isSearchFocused");
   isHomePage = this.globalStateService.select("isHomePage");
   travelStateService = inject(TravelStateService);
   mapService = inject(MapService);
 
-  addressList: IAddress[] = [];
   markers: IMarker[] | undefined = this.travelStateService.getMarkers();
-  isTravelRoute$: Observable<boolean> = of(true);
-
-
-  getLookupAddress(addresses: IAddress[]) {
-    this.addressList = addresses;
-  }
 
   onSelectedAddress(address: IAddress): void {
     const { lat, lng } = address;
     const position: IGpsPosition = { lat, lng };
-    this.globalStateService.set("isSearchFocused", false);
     this.mapService.addMarkerAndSetView(position);
   }
 
-  closeSearchPanel() {
-    this.globalStateService.set("isSearchFocused", false);
-  }
 }
