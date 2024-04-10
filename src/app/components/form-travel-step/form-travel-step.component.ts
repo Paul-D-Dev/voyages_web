68ef9f935562df769da9e3283d5cdfe77b3cb6bc
Paul DAO <dao.van.paul@gmail.com>
2024-04-10 15:34:43 -0400

    REFACTOR form travel step component init and update form

--- src/app/components/form-travel-step/form-travel-step.component.ts
+++ src/app/components/form-travel-step/form-travel-step.component.ts
@@ -13,14 +13,13 @@ import { MatIcon } from "@angular/material/icon";
 import { MatInput } from "@angular/material/input";
 import { MatMiniFabButton } from "@angular/material/button";
 import { ITravelStep, ITravelStepFormData } from "../../shared/interfaces/travel.interface";
-import { IGpsPosition } from "../../shared/interfaces/gps-position.interface";
 import {
   MatAutocomplete,
   MatAutocompleteSelectedEvent,
   MatAutocompleteTrigger,
   MatOption
 } from "@angular/material/autocomplete";
-import { debounceTime, Observable, of, startWith, switchMap } from "rxjs";
+import { debounceTime, Observable, startWith, switchMap } from "rxjs";
 import { AddressService } from "../../shared/services/address.service";
 import { IAddress } from "../../shared/interfaces/address.interface";
 import { AsyncPipe, KeyValuePipe, TitleCasePipe } from "@angular/common";
@@ -82,7 +81,25 @@ import { animate, state, style, transition, trigger } from "@angular/animations"
 export class FormTravelStepComponent {
   @Input() set formData(data: ITravelStep | undefined) {
     if (!data) return;
-    this._stepData = data;
+    const {
+      label,
+      description,
+      category,
+      dateStart,
+      dateEnd,
+      location
+    } = data;
+
+    const formattedData: ITravelStepFormData = {
+      label,
+      category,
+      description,
+      dateStart: this._formatDateLocalTime(dateStart),
+      dateEnd: this._formatDateLocalTime(dateEnd),
+      location
+    };
+
+    this.form.patchValue({ ...formattedData, locationAddress: '' });
   }
 
   @Output() onSubmitForm = new EventEmitter<ITravelStepFormData>;
@@ -91,39 +108,27 @@ export class FormTravelStepComponent {
   private readonly fb = inject(FormBuilder);
   protected readonly Icons = Icons;
   protected readonly Categories = StepCategories;
-  addresses$: Observable<IAddress[]> = of([]);
-  isDisplayedLocationInput = false;
-
   // TODO complete FormGroup type
-  form!: FormGroup;
-  private _stepData: ITravelStepFormData = {
-    label: '',
-    category: null,
-    dateStart: this._formatDateLocalTime(new Date().toISOString()),
-    dateEnd: this._formatDateLocalTime(new Date().toISOString()),
-    location: {} as IGpsPosition
-  };
+  form: FormGroup = this.fb.group({
+    label: ['', { validators: [Validators.required] }],
+    description: [''],
+    dateStart: [this._formatDateLocalTime(new Date()), { validators: [Validators.required] }],
+    dateEnd: [this._formatDateLocalTime(new Date()), { validators: [Validators.required] }],
+    category: [null],
+    locationAddress: [''],
+    location: this.fb.group({
+      lng: [null, { validators: [Validators.required] }],
+      lat: [null, { validators: [Validators.required] }]
+    })
+  });
 
-  ngOnInit() {
-    this.form = this.fb.group({
-      label: [this._stepData.label, { validators: [Validators.required] }],
-      description: [this._stepData.description],
-      dateStart: [this._stepData.dateStart, { validators: [Validators.required] }],
-      dateEnd: [this._stepData.dateEnd, { validators: [Validators.required] }],
-      category: [this._stepData.category],
-      locationAddress: [''],
-      location: this.fb.group({
-        lng: [this._stepData.location.lng, { validators: [Validators.required] }],
-        lat: [this._stepData.location.lat, { validators: [Validators.required] }]
-      })
-    });
+  addresses$: Observable<IAddress[]> = this.form.controls['locationAddress'].valueChanges.pipe(
+    startWith(''),
+    debounceTime(1000),
+    switchMap((value) => this.addressService.lookUp(value)),
+  );
 
-    this.addresses$ = this.form.controls['locationAddress'].valueChanges.pipe(
-      startWith(''),
-      debounceTime(1000),
-      switchMap((value) => this.addressService.lookUp(value)),
-    );
-  }
+  isDisplayedLocationInput = false;
 
   displayFn(address: IAddress): string {
     return address && address.label ? address.label : '';
@@ -155,7 +160,8 @@ export class FormTravelStepComponent {
     }
   }
 
-  private _formatDateLocalTime(dateIsoString: string): string {
-    return dateIsoString.substring(0, 16);
+  private _formatDateLocalTime(date: Date | string): string {
+    const dateString = typeof date === 'string' ? date : date.toISOString();
+    return dateString.substring(0, 16);
   }
 }
