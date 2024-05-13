3cc4ddb5bfe948eba212691cfb6aaa1c56d56689
Paul DAO <dao.van.paul@gmail.com>
2024-05-13 15:28:41 -0400

    FEAT overlay-travel-detail add navigateTo

--- src/app/components/overlay-travel-detail/overlay-travel-detail.component.ts
+++ src/app/components/overlay-travel-detail/overlay-travel-detail.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { MatIconButton } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
 import { Icons } from "../../shared/enums/icons.enum";
+import { IGpsPosition } from "../../shared/interfaces/gps-position.interface";
+import { OpenOnMapsAppService } from "../../shared/services/open-on-maps-app.service";
+import { ITravelStep } from "../../shared/interfaces/travel.interface";
 
 @Component({
   selector: 'app-overlay-travel-detail',
@@ -14,5 +17,13 @@ import { Icons } from "../../shared/enums/icons.enum";
   styleUrl: './overlay-travel-detail.component.scss'
 })
 export class OverlayTravelDetailComponent {
+  @Input() travelDetail: ITravelStep | undefined = undefined;
+  openOnMapsAppService = inject(OpenOnMapsAppService);
   protected readonly Icons = Icons;
+
+  navigateTo(location: IGpsPosition | undefined): void {
+    if (location) {
+      this.openOnMapsAppService.viewLocation(location);
+    }
+  }
 }
