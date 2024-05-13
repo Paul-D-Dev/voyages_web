ddaf68d0413249adfd705135d07fca21646e2167
Paul DAO <dao.van.paul@gmail.com>
2024-05-13 16:28:37 -0400

    FEAT navigate service goHomeIfMobile()

--- src/app/shared/services/navigation.service.ts
+++ src/app/shared/services/navigation.service.ts
@@ -1,6 +1,7 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, Signal } from '@angular/core';
 import { Location } from "@angular/common";
 import { NavigationExtras, Router } from "@angular/router";
+import { GlobalStateService } from "./global-state.service";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,7 @@ import { NavigationExtras, Router } from "@angular/router";
 export class NavigationService {
   location = inject(Location);
   router = inject(Router);
+  isMobile: Signal<boolean> = inject(GlobalStateService).select('isMobile');
 
   back() {
     this.location.back();
@@ -19,6 +21,14 @@ export class NavigationService {
 
   go(path: string[], options?: NavigationExtras) {
     this.router.navigate(path, options);
+  }
+
+  goHomeIfMobile() {
+    if (this.isMobile()) {
+      this.router.navigate(['/']);
+    } else {
+      console.warn('The display screen is not a mobile then the user can not navigate to home page')
+    }
   }
 
 }
