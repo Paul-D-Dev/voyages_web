6632da65cd6f71e1abde3f23314907f61d7a4cbf
Paul DAO <dao.van.paul@gmail.com>
2024-07-05 15:13:33 -0400

    WIP file upload dialog change UI

--- src/app/components/dialogs/file-upload-dialog/file-upload-dialog.component.ts
+++ src/app/components/dialogs/file-upload-dialog/file-upload-dialog.component.ts
@@ -10,6 +10,8 @@ import { MatFormField, MatLabel } from "@angular/material/form-field";
 import { MatOption, MatSelect } from "@angular/material/select";
 import { MatButton } from "@angular/material/button";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatIcon } from "@angular/material/icon";
+import { Icons } from "../../../shared/enums/icons.enum";
 
 @Component({
   selector: 'app-file-upload-dialog',
@@ -24,7 +26,8 @@ import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
     MatDialogActions,
     MatButton,
     MatDialogClose,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatIcon
   ],
   templateUrl: './file-upload-dialog.component.html',
   styleUrl: './file-upload-dialog.component.scss'
@@ -33,6 +36,7 @@ export class FileUploadDialogComponent {
   constructor(public dialogRef: MatDialogRef<FileUploadDialogComponent>, private _fb: FormBuilder) {
   }
 
+  isFocused = false;
   documentForm = this._fb.group({
     type: ['', Validators.required],
     name: ['', Validators.required],
@@ -43,7 +47,6 @@ export class FileUploadDialogComponent {
 
   // TODO refactor event type
   onFileChange(event: any): void {
-    console.log(event);
     const file: File = event.target.files[0];
     if (file) {
       const reader = new FileReader();
@@ -59,7 +62,18 @@ export class FileUploadDialogComponent {
     }
   }
 
+  onBlur(): void {
+    this.isFocused = false;
+  }
+
+  onFocus(): void {
+    this.isFocused = true;
+  }
+
+
   onClose(): void {
     this.dialogRef.close();
   }
+
+  protected readonly Icons = Icons;
 }
